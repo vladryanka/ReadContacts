2051b38941168e49246b6667d61090f4778b99e5
vladryanka <vsmorzhok@mail.ru>
2025-03-23 22:45:40 +0300

    Получение разрешения от пользователя

--- app/src/main/java/com/smorzhok/readcontacts/MainActivity.kt
+++ app/src/main/java/com/smorzhok/readcontacts/MainActivity.kt
@@ -20,8 +20,32 @@ class MainActivity : AppCompatActivity() {
         if (permissionGranted) {
             requestContacts()
         } else {
-            Log.d("MainActivity", "Permission denied")
+            requestPermission()
         }
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.READ_CONTACTS),
+            READ_CONTACTS_RC
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == READ_CONTACTS_RC && grantResults.isNotEmpty()) {
+            val permissionGranted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+            if (permissionGranted) {
+                requestContacts()
+            } else {
+                Log.d("MainActivity", "Permission denied")
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     private fun requestContacts() {
@@ -45,5 +69,9 @@ class MainActivity : AppCompatActivity() {
             }
             cursor?.close()
         }
+    }
+
+    companion object {
+        const val READ_CONTACTS_RC = 1
     }
 }
