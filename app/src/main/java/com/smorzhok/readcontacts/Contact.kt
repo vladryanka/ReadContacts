dc9299ac134caa773c46142a90cc0c3f4ee02fec
vladryanka <vsmorzhok@mail.ru>
2025-03-23 22:10:23 +0300

    Initial commit

---
+++ app/src/main/java/com/smorzhok/readcontacts/Contact.kt
@@ -0,0 +1,6 @@
+package com.smorzhok.readcontacts
+
+data class Contact(
+    val id: Int,
+    val name: String
+)
---
+++ app/src/main/java/com/smorzhok/readcontacts/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.smorzhok.readcontacts
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import kotlin.concurrent.thread
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val permissionGranted = ActivityCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.READ_CONTACTS
+        ) == PackageManager.PERMISSION_GRANTED
+
+        if (permissionGranted) {
+            requestContacts()
+        } else {
+            Log.d("MainActivity", "Permission denied")
+        }
+    }
+
+    private fun requestContacts() {
+        thread {
+            val cursor = contentResolver.query(
+                ContactsContract.Contacts.CONTENT_URI,
+                null,
+                null,
+                null,
+                null
+            )
+            while (cursor?.moveToNext() == true) {
+                val id = cursor.getInt(
+                    cursor.getColumnIndexOrThrow(ContactsContract.Contacts._ID)
+                )
+                val name = cursor.getString(
+                    cursor.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME)
+                )
+                val contact = Contact(id, name)
+                Log.d("MainActivity", contact.toString())
+            }
+            cursor?.close()
+        }
+    }
+}
